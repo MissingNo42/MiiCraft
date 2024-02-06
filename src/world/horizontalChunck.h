a328932ea144a5e0341e68f1a1d174c03640fb0e
Guillaume Assailly <guillaume.assailly@etu.uca.fr>
2024-02-06 11:04:01 +0100

    Début de la partie métier avec nos supers structures !!!

--- src/world/horizontalChunck.h
+++ src/world/horizontalChunck.h
@@ -5,4 +5,15 @@
 #ifndef MIICRAFT_HORIZONTALCHUNCK_H
 #define MIICRAFT_HORIZONTALCHUNCK_H
 
+#include "coord.h"
+#include "block.h"
+#include <vector>
+
+
+
+class HorizontalChunk {
+private:
+    std::vector<Block> blocks;
+};
+
 #endif //MIICRAFT_HORIZONTALCHUNCK_H
--- src/world/verticalChunk.h
+++ src/world/verticalChunk.h
@@ -5,4 +5,23 @@
 #ifndef MIICRAFT_VERTICALCHUNK_H
 #define MIICRAFT_VERTICALCHUNK_H
 
+#include "coord.h"
+#include "horizontalChunck.h"
+
+class VerticalChunk {
+
+private:
+    int index;
+    t_coord coords;
+    std::vector<HorizontalChunk> chunks;
+public:
+
+
+};
+
+
 #endif //MIICRAFT_VERTICALCHUNK_H
+
+
+
+
