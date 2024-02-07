4eaea125975d08cf8794bbcb0e0a7a6b4d8d5d27
JeNeVoulaisPas <147522431+JeNeVoulaisPas@users.noreply.github.com>
2024-02-07 12:34:11 +0100

    Fix : Coord Constr

--- src/world/coord.h
+++ src/world/coord.h
@@ -6,11 +6,7 @@
 #define WII_COORD_H
 
 struct t_coord{
-    t_coord(int x, int y, int z){
-        this->x = x;
-        this->y = y;
-        this->z = z;
-    }
+    t_coord(int x, int y, int z);
     int x;
     int y;
     int z;
