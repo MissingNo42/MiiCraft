958f523ee63d2d5eb344c3b3c7a44cf3583e4371
Guillaume Assailly <guillaume.assailly@etu.uca.fr>
2024-02-07 09:49:10 +0100

    Ajout du générateur de perlin pinpin

--- src/world/FlatWorldGenerator.cpp
+++ src/world/FlatWorldGenerator.cpp
@@ -4,26 +4,9 @@
 
 #include "FlatWorldGenerator.h"
 
-BlockType FlatWorldGenerator::guessBlockAt(const t_coord pos) {
-    if(pos.y > 64){
-        return BlockType::Air;
-    }
-    if(pos.y == 64){
-        return BlockType::Grass;
-    }
-    if(pos.y < 64 && pos.y > 32 ){
-        return BlockType::Dirt;
-    }
-    if(pos.y <= 32){
-        return BlockType::Stone;
-    }
-    return BlockType::Air;
-}
-
-void FlatWorldGenerator::generateChunk(World w, const t_pos2D pos) {
 
 
 
 
 
-}
+
