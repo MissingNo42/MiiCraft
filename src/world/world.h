1364a57afbcd38840c0dd35192475e86577b0e67
JeNeVoulaisPas <147522431+JeNeVoulaisPas@users.noreply.github.com>
2024-02-07 09:57:08 +0100

    Add : GetBlock State : Generation Work Theoretically

--- src/world/world.h
+++ src/world/world.h
@@ -20,7 +20,7 @@ public:
     Block getBlockAt(t_coord coord);
     void setBlockAt(t_coord coord, BlockType block);
 
-    t_pos2D to_chunk_pos(t_coord& c);
+    static t_pos2D to_chunk_pos(t_coord& c);
     VerticalChunk& getChunkAt(t_pos2D pos);
     void addChunk(t_pos2D pos, VerticalChunk* chunk);
 };
